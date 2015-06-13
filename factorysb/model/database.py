c7a75256a0576432cb6709107bc39b4c3e051720
rosspalmer <rossbpalmer@gmail.com>
2015-06-13 15:32:36 -0600

    computer switch

--- factorysb/model/database.py
+++ factorysb/model/database.py
@@ -1,42 +1,39 @@
 import pandas as pd
+import random as rn
 from sqlalchemy import create_engine
 from pandas.io.sql import read_sql
 
 class data(object):
 
     def __init__(self):
-        self.lines = pd.DataFrames(columns=['name','recipe','rate'],
-                        index=['name','recipe'])
-        self.store = pd.DataFrame(columns=['name','product','initial','limit'],
-                        index=['name','product'])
-        self.recipes = pd.DataFrame(columns=['name','product','amount','ratio',
-                        'output'],index=['name','product'])
-        self.conns = pd.DataFrame(columns=['name','output','product','rate',
-                        'workers'],index=['input','output','product'])
-
-class sql(object):
-
-    def __init__(self):
-        self.eng = self.engine()
+        self.eng = create_engine('sqlite:///fsb.db')
         self.db = {}
-
-    def engine(self):
-        eng = create_engine('sqlite:///fsb.db')
-        return eng
+        self.db['lines'] = pd.DataFrame(columns=['name','recipe','rate'],
+                        index=['name','recipe'])
+        self.db['stores'] = pd.DataFrame(columns=['name','product','initial','limit'],
+                        index=['name','product'])
+        self.db['recipes'] = pd.DataFrame(columns=['name','product','amount','ratio',
+                        'output'],index=['name','product'])
+        self.db['conns'] = pd.DataFrame(columns=['input','output','product','rate',
+                        'workers'],index=['input','output','product'])
 
     def load(self):
-        db = {}
-        tables = ['lines','stores','recipes','connections']
-        for table in tables:
-            db[table] = read_sql(tables, self.eng)
-        return db
+        for table in self.db.keys():
+            self.db[table] = read_sql(table, self.eng)
 
-    def create(self):
-        self.lines = pd.DataFrames(columns=['name','recipe','rate'],
-                        index=['name','recipe'])
-        self.store = pd.DataFrame(columns=['name','product','initial','limit'],
-                        index=['name','product'])
-        self.recipes = pd.DataFrame(columns=['name','product','amount','ratio',
-                        'output'],index=['name','product'])
-        self.conns = pd.DataFrame(columns=['name','output','product','rate',
-                        'workers'],index=['input','output','product'])
+    def update(self):
+        for table in self.db.keys():
+            self.db[table].to_sql(table, self.eng, if_exists='replace')
+
+    def create_xl(self):
+        writer = pd.ExcelWriter('fsb.xlsx')
+        for table in self.db.keys():
+            self.db[table].to_excel(writer, table)
+
+d = data()
+for i in range(10):
+    row = {'name':rn.randint(1,4),'recipe':rn.randint(1,10),'rate':rn.randint(100,200)}
+    d.db['lines'] = d.db['lines'].append(row, ignore_index='True')
+
+print d.db['lines']
+
